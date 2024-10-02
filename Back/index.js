af0a0dc51ac9f6ec36ef1fe44176c2a16615dfdc
Elenadelatorre <elenadelatorre1806@gmail.com>
2024-10-02 10:33:55 +0200

    Vercel

--- Back/index.js
+++ Back/index.js
@@ -27,7 +27,7 @@ app.use('/api/v1/asistentes', asistentesRouter);
 app.use('/api/v1/users', usersRouter);
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://proyecto-eventos-two.vercel.app',
   optionsSuccessStatus: 200
 };
 
